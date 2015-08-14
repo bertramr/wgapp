aa68280ca0b46d9e22e678bd723f70d719594959
rb570596 <bertram@ifht.rwth-aachen.de>
2015-08-14 21:25:06 +0200

    added admin support

--- wgapp/admin.py
+++ wgapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Flatmate, TaskJournal, TaskList
+
+admin.site.register(Flatmate)
+admin.site.register(TaskJournal)
+admin.site.register(TaskList)
--- wgapp/models.py
+++ wgapp/models.py
@@ -7,10 +7,15 @@ class Flatmate(models.Model):
     short_name = models.CharField(max_length=2)
     full_name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.full_name
+
 
 class TaskList(models.Model):
     description = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.description
 
 class TaskJournal(models.Model):
     done_by = models.ForeignKey(Flatmate)
