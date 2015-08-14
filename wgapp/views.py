cd9ecc8a204da8c5eff55b43438b0930a137e1b6
rb570596 <bertram@ifht.rwth-aachen.de>
2015-08-14 21:50:13 +0200

    added first template

--- wgapp/views.py
+++ wgapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from models import TaskJournal
+
+def index(request):
+    latest_tasks_list = TaskJournal.objects.order_by('-done_on')[:5]
+    context = {'latest_tasks_list': latest_tasks_list}
+    return render(request, 'wgapp/index.html', context)
