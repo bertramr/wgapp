21e70b5259dea26fe11d3921f2592d825c0aae39
rb570596 <bertram@ifht.rwth-aachen.de>
2015-08-17 13:19:37 +0200

    experiments with views

--- development/urls.py
+++ development/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^wgapp/', include('wgapp.urls')),
+    url(r'^wgapp/', include('wgapp.urls', namespace="wgapp")),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- wgapp/views.py
+++ wgapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-from models import TaskJournal
+from models import TaskJournal, Flatmate
+
 
 def index(request):
+    flatmate_list = Flatmate.objects.all()
     latest_tasks_list = TaskJournal.objects.order_by('-done_on')[:5]
-    context = {'latest_tasks_list': latest_tasks_list}
+    context = {'latest_tasks_list': latest_tasks_list,
+               'flatmate_list': flatmate_list}
     return render(request, 'wgapp/index.html', context)
+
+
+def detail(request, flatmate_id):
+    context = {'flatmate': Flatmate.objects.get(pk=flatmate_id)}
+    return render(request, 'wgapp/detail.html', context)
