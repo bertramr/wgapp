fbdee746e40d4c7efdb6350df2717e612a7b1be2
rb570596 <bertram@ifht.rwth-aachen.de>
2015-08-23 21:11:25 +0200

    added room views and choose flatmate

--- wgapp/urls.py
+++ wgapp/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<flatmate_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^room/$', views.room, name='room'),
+    url(r'^room/(?P<room_id>[0-9]+)', views.room_detail, name='room_detail'),
+    url(r'^task/(?P<task_id>[0-9]+)/', views.choose_flatmate, name='choose_flatmate')
 ]
--- wgapp/views.py
+++ wgapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from models import TaskJournal, Flatmate
+from models import TaskJournal, Flatmate, Room, TaskList
 
 
 def index(request):
@@ -14,3 +14,22 @@ def index(request):
 def detail(request, flatmate_id):
     context = {'flatmate': Flatmate.objects.get(pk=flatmate_id)}
     return render(request, 'wgapp/detail.html', context)
+
+
+def room(request):
+    room_list = Room.objects.all()
+    context = {'room_list': room_list}
+    return render(request, 'wgapp/room.html', context)
+
+
+def room_detail(request, room_id):
+    task_list = TaskList.objects.filter(room=room_id).all()
+    context = {'task_list': task_list}
+    return render(request, 'wgapp/room_detail.html', context)
+
+def choose_flatmate(request, task_id):
+    flatmate_list = Flatmate.objects.all()
+    task = TaskList.objects.get(pk=task_id)
+    context = {'flatmate_list': flatmate_list,
+               'task': task}
+    return render(request, 'wgapp/choose_flatmate.html', context)
